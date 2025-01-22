3de0803fbd71b4062cd2d5ab70818bfa31d615bc
Tatyana Krasnova <krasnova1987art@yandex.ru>
2025-01-22 22:25:13 +0300

    backend

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -16,6 +16,7 @@ const onClickAdd = () => {
     <div class="grid grid-cols-4 gap-5">
         <Card 
         v-for="item in items"
+            :key="item.id"
             :title="item.title" 
             :image-url="item.imageUrl" 
             :price="item.price" 
@@ -24,4 +25,5 @@ const onClickAdd = () => {
             :onClickAdd = "onClickAdd"
         />
     </div>
+
 </template>
